63ceffc9936866e3e17877ab11e7b2c6003a4a1e
Matteo Scandolo <teo@opennetworking.org>
2018-06-12 13:36:58 -0700

    [CORD-3022] Splitting config
    Change-Id: If53017fb35e41ea0536afa14369bd525549c019c

--- xos/synchronizer/vtn-synchronizer.py
+++ xos/synchronizer/vtn-synchronizer.py
@@ -21,8 +21,13 @@ import os
 import sys
 from xosconfig import Config
 
-config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/vtn_config.yaml')
-Config.init(config_file, 'synchronizer-config-schema.yaml')
+base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
+
+if os.path.isfile(mounted_config_file):
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
+else:
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml')
 
 sys.path.append('/opt/xos')
 
